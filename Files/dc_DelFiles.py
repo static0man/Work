6726503532d576372dba4897b28ab14d8611ea45
static0man <741007641@qq.com>
2019-10-28 19:59:44 +0800

    删除指定路径的文件

---
+++ Files/dc_DelFiles.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# coding=utf-8
+# ----------------------
+# @Time    : 2019/10/28 19:55
+# @Author  : hwf
+# @File    : dc_DelFiles1.py
+# ----------------------
+
+import sys
+import os
+
+Path = ""
+Name = ""
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
